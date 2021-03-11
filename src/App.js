06f29dfcf8d6577099261e72f70101ccaafc01f2
Nathan <nathan.ar.94@gmail.com>
2021-03-10 21:13:52 -0300

    fix: component login excluded

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Login from "./components/login";
 import Menu from "./components/Menu";
+import Routes from "./routes";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Login />
+        <Routes />
       </header>
     </div>
   );
